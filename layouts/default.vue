56617fcb3ed270a023322e326dcbfb852719b536
davidPesava <pesava.david@gmail.com>
2020-02-21 18:55:24 +0100

    Clean up layout - default

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,32 +11,3 @@
     </v-footer>
   </v-app>
 </template>
-
-<script>
-
-export default {
-  data () {
-    return {
-      clipped: false,
-      drawer: true,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: this.$store.state.users.currentUser ? 'mdi-chart-bubble' : '',
-          title: this.$store.state.users.currentUser ? 'Dashboard' : '',
-          to: this.$store.state.users.currentUser ? '/db' : '',
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Auth module'
-    }
-  },
-}
-</script>
