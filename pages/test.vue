0291daf8806e34ff7b01835e553190c16f5d2f5c
David <pesava.david@gmail.com>
2020-02-26 19:48:11 +0100

    Add sample test page

---
+++ pages/test.vue
@@ -0,0 +1,45 @@
+<template>
+	<div>
+       <div v-for="test in poleA">
+           <div>{{test.name}}</div>
+           <div v-for="company in test.history">
+               <div><pre>{{company}}</pre></div>
+           </div>
+       </div>
+    </div>
+</template>
+
+<script>
+
+
+		export default {
+                
+                layout: 'app-layout',
+				created: function () { 
+					
+				},
+				methods: {
+		
+				},
+				data() {
+						return {
+                            poleA: [ // chatHistoryData
+                                { //outer helper
+                                    name: "company",
+                                    history: [
+                                         [7, 30, 0],
+                                         [17, 30, 0]
+                                    ]
+                                },
+                                {
+                                    name: "company",
+                                    history: [
+                                         [7, 30, 0],
+                                         [17, 30, 0]
+                                    ]
+                                }                               
+                            ],
+						}
+				},
+		}
+</script>
