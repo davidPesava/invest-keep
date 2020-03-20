3b3bbd70d58ca6bbb1400a6421203cf04a16977a
davidPesava <pesava.david@gmail.com>
2020-03-20 18:19:30 +0100

    Add dashboards rendering

--- components/dashboards/primary.vue
+++ components/dashboards/primary.vue
@@ -1,8 +1,9 @@
 <template>
 	<div>
 		<h2 class="mb-4">Primary dashboard</h2>
-		<div v-for="graph in graphTypes">
-			<GChart :type="graph" :data="chatLoadedData" :options="chartOptions" class="mb-5"/>  
+		<div v-for="graph in graphs">
+			<h3><strong class="primary--text">{{ graph.name }}</strong></h3>
+			<GChart :type="graph.graphType" :data="graph.data" :options="chartOptions" class="mb-5"/>  
 		</div>
 	</div>
 </template>
@@ -13,14 +14,17 @@
 		name: "primary",
 		components: {GChart},
 		props: {
-			chatLoadedData: {
-				type: Array,
-			},
-			graphTypes: {
-				type: Array,
-			},
-			chartOptions: {
-				type: Object,
+			graphs: {
+				type: Array
+			}
+		},
+		data() {
+			return {
+				chartOptions: {
+							crosshair: {trigger: 'both'},
+							width: 1000,
+
+				}				
 			}
 		}
 	}
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -11,9 +11,9 @@
 			<v-tab @click="changeToSecondary()">Secondary</v-tab>
 			<v-tab @click="changeToEarnings()">Earnings</v-tab>
 		</v-tabs>
-		<primary v-if="dashboards.primary" :chatLoadedData="chatLoadedData" :graphTypes="graphTypes" />
-		<secondary v-if="dashboards.secondary" :chatLoadedData="chatLoadedData" :graphTypes="graphTypes" />
-		<earnings v-if="dashboards.earnings" :symbols = symbols />
+		<primary v-if="dashboards.primary" :graphs="fetchedDashboardsData.primary" />
+		<!--<secondary v-if="dashboards.secondary" :chatLoadedData="chatLoadedData" :graphTypes="graphTypes" />
+		<earnings v-if="dashboards.earnings" :symbols = symbols /> -->
 	</v-layout>
 </template>
 
@@ -28,8 +28,8 @@
 				layout: 'app-layout',
 				middleware: 'router-auth',
 				components: {GChart, primary, secondary, earnings},
-				created: function () { 
-					this.initStocks()
+				created: function () {
+						this.initStocks()
 				},
 				methods: {
 					initStocks(){
@@ -42,29 +42,149 @@
 							 } else {
 								let val = JSON.parse(JSON.stringify(doc.data()))
 								this.$store.commit('users/setCredentials', val)	
-								//Fetch all stock to render into graph
-								this.fetchStocks(val.stocks) 
-								//Fetch users stocks to be used next
-								this.symbols = val.stocks
+
+								let arrayGraphs = val.primary
+								arrayGraphs.forEach((element) => {
+									let localGraphsArray = this.fetchGraph(element).then(
+									singleGraph => {
+										this.fetchedDashboardsData.primary.push(singleGraph)
+										console.log(this.fetchedDashboardsData.primary)
+									}
+									)
+								})
+
+
+								
 							 }
 						 })
 					},
-					async fetchStocks(usersSymbols) {
-						let symbols = usersSymbols
-						const fetchedStocks = await this.$axios.$get(this.$store.state.config.env.baseApiUrl+'stock?symbol='+symbols+ '&api_token='+this.$store.state.config.env.apiToken)
-						let localArrayNames = ['Name','Price actual','Price - Highest of the day','Price - lowest of the day', 'Price open - today']
-						this.chatLoadedData.push(localArrayNames)
-						fetchedStocks.data.forEach((element, index, array) => {
+					async fetchGraph(graph) {
+						let symbol = graph.symbol
+						let graphOptions = graph
+						let outerHelper = {}
+						outerHelper.graphType = graphOptions.graph
+						outerHelper.name = graphOptions.name
+						outerHelper.data = []
+						let descriptions = ["Name"]
+						if(graphOptions.price) {
+							descriptions.push("Actual price")
+						}
+						if(graphOptions.priceOpen) {
+							descriptions.push("Price open")
+						}
+						if(graphOptions.dayHigh) {
+							descriptions.push("Day high")
+						}
+						if(graphOptions.dayLow) {
+							descriptions.push("Day low")
+						}
+						if(graphOptions.high52) {
+							descriptions.push("52 weeks high")
+						}
+						if(graphOptions.low52) {
+							descriptions.push("52 weeks low")
+						}						
+						if(graphOptions.dayChange) {
+							descriptions.push("Day change")
+						}
+						if(graphOptions.changePct) {
+							descriptions.push("Change pct")
+						}						
+						if(graphOptions.closeY) {
+							descriptions.push("Close yeasterday")
+						}
+						if(graphOptions.marketCap) {
+							descriptions.push("Market cap")
+						}												
+						if(graphOptions.volume) {
+							descriptions.push("Volume")
+						}
+						if(graphOptions.volume_avg) {
+							descriptions.push("Avarage volume")
+						}
+						if(graphOptions.shares) {
+							descriptions.push("Shares")
+						}
+						if(graphOptions.pe) {
+							descriptions.push("PE ratio")
+						}
+						if(graphOptions.eps) {
+							descriptions.push("EPS ratio")
+						}												
+						if(graphOptions.currency) {
+							descriptions.push("Currency")
+						}
+						if(graphOptions.exchange) {
+							descriptions.push("Stock exchange")
+						}
+						if(graphOptions.timezone) {
+							descriptions.push("Time")
+						}
+						outerHelper.data.push(descriptions)
+						const fetchedStocks = await this.$axios.$get(this.$store.state.config.env.baseApiUrl+'stock?symbol='+symbol+ '&api_token='+this.$store.state.config.env.apiToken)
+						console.log(fetchedStocks)
+						fetchedStocks.data.forEach((element) => {
 							let helper = []
-							helper.push(element.name + '(' + element.symbol + ')')
-							helper.push(parseFloat(element.price))
-							helper.push(parseFloat(element.day_high))
-							helper.push(parseFloat(element.day_low))
-							helper.push(parseFloat(element.price_open))
-							this.companies.push(element.name)
-							this.chatLoadedData.push(helper)
-						});
-					},changeToPrimary() {
+							helper.push(element.name)
+							if(graphOptions.price) {
+								helper.push(parseFloat(element.price))
+							}
+							if(graphOptions.priceOpen) {
+								helper.push(parseFloat(element.price_open))
+							}
+							if(graphOptions.dayHigh) {
+								helper.push(parseFloat(element.day_high))
+							}
+							if(graphOptions.dayLow) {
+								helper.push(parseFloat(element.day_low))
+							}
+							if(graphOptions.high52) {
+								helper.push(parseFloat(element['52_week_high']))
+							}
+							if(graphOptions.low52) {
+								helper.push(parseFloat(element['52_week_low']))
+							}						
+							if(graphOptions.dayChange) {
+								helper.push(parseFloat(element.day_change))
+							}
+							if(graphOptions.changePct) {
+								helper.push(parseFloat(element.change_pct))
+							}						
+							if(graphOptions.closeY) {
+								helper.push(parseFloat(element.close_yesterday))
+							}
+							if(graphOptions.marketCap) {
+								helper.push(parseFloat(element.market_cap))
+							}												
+							if(graphOptions.volume) {
+								helper.push(parseFloat(element.volume))
+							}
+							if(graphOptions.volume_avg) {
+								helper.push(parseFloat(element.volume_avg))
+							}
+							if(graphOptions.shares) {
+								helper.push(parseFloat(element.shares))
+							}
+							if(graphOptions.pe) {
+								helper.push(parseFloat(element.pe))
+							}
+							if(graphOptions.eps) {
+								helper.push(parseFloat(element.eps))
+							}												
+							if(graphOptions.currency) {
+								helper.push(parseFloat(element.currency))
+							}
+							if(graphOptions.exchange) {
+								helper.push(parseFloat(element.stock_exchange_long))
+							}
+							if(graphOptions.timezone) {
+								helper.push(parseFloat(element.timezone_name))
+							}
+							outerHelper.data.push(helper)
+						})
+						return outerHelper
+					},
+					changeToPrimary() {
 						this.dashboards.primary = true
 						this.dashboards.secondary = false
 						this.dashboards.earnings = false
@@ -87,6 +207,12 @@
 								secondary: false,
 								earnings: false,
 							},
+							dashboardsData: {
+								primary: [],
+							},
+							fetchedDashboardsData: {
+								primary: [],
+							},
 							graphTypes: ['Table','BarChart','LineChart','ColumnChart','Histogram','AreaChart','PieChart','BubbleChart','CandlestickChart','SteppedAreaChart','ScatterChart'],
 							currentUser: '',
 							symbols: '',
@@ -96,7 +222,7 @@
 								base: {
 									title: 'Base Stocks prices',
 									crosshair: {trigger: 'both'},
-									width: 900,
+									width: 500,
 								}
 							} 
 						}
