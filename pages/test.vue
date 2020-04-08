fc31f0d8b1d13f1459510eb1a6b5da23ff56a723
David <pesava.david@gmail.com>
2020-04-08 21:47:26 +0200

    Add playground

--- pages/test.vue
+++ pages/test.vue
@@ -1,12 +1,22 @@
 <template>
-	<div>
-       <div v-for="test in poleA">
-           <div>{{test.name}}</div>
-           <div v-for="company in test.history">
-               <div><pre>{{company}}</pre></div>
-           </div>
-       </div>
+<div>
+    <div id='example-3'>
+        <div v-for="symbol in symbols">
+            {{symbol}}
+            <input type="checkbox"  :value="symbol" v-model="checked.symbols">
+        
+        </div>
+        <input type="checkbox" id="jack" value="Jack" v-model="checked.symbols">
+        <label for="jack">Jack</label>
+        <input type="checkbox" id="john" value="John" v-model="checked.symbols">
+        <label for="john">John</label>
+        <input type="checkbox" id="mike" value="true" v-model="checked.graph1">
+        <label for="mike">Mike</label>
+        <br>
+        <span>Checked names: {{ checked }}</span>
     </div>
+</div>
+
 </template>
 
 <script>
@@ -23,22 +33,11 @@
 				},
 				data() {
 						return {
-                            poleA: [ // chatHistoryData
-                                { //outer helper
-                                    name: "company",
-                                    history: [
-                                         [7, 30, 0],
-                                         [17, 30, 0]
-                                    ]
-                                },
-                                {
-                                    name: "company",
-                                    history: [
-                                         [7, 30, 0],
-                                         [17, 30, 0]
-                                    ]
-                                }                               
-                            ],
+                            symbols: ["F","TWTR","R"],
+                            checked: {
+                                symbols: [],
+                                graph1: false,
+                            }
 						}
 				},
 		}
